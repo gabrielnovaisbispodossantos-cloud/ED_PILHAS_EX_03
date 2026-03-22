13b4b2ad319692d2dd29801b18de7d4f7b15157d
Gabriel Novais Bispo dos Santos <gabrielnovaisbispodossantos@gmail.com>
2026-03-22 15:04:00 -0300

    Add files via upload

---
+++ pilhaint/No.java
@@ -0,0 +1,8 @@
+package br.gui.pilhaint;
+
+public class No { // Objeto
+	
+	int dado;
+	No proximo; // Ponteiro // tipo "ele mesmo"
+	
+}
---
+++ pilhastring/No.java
@@ -0,0 +1,8 @@
+package br.gui.pilhastring;
+
+public class No { // Objeto
+	
+	String dado;
+	No proximo; // Ponteiro // tipo "ele mesmo"
+	
+}
